e2214c481801b8665803aa0fd61cf0f66463bd34
23rd <23rd@vivaldi.net>
2018-10-17 15:13:07 +0300

    Changed notification icon.

--- TMessagesProj/src/main/java/org/telegram/messenger/NotificationsController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/NotificationsController.java
@@ -4587,7 +4587,7 @@ public class NotificationsController extends BaseController implements Notificat
                     .setGroupSummary(true)
                     .setShowWhen(true)
                     .setWhen(((long) lastMessageObject.messageOwner.date) * 1000)
-                    .setColor(0xff11acfa);
+                    .setColor(0xff3A2E5C);
 
             long[] vibrationPattern = null;
             Uri sound = null;
@@ -5583,7 +5583,7 @@ public class NotificationsController extends BaseController implements Notificat
                     .setContentText(text.toString())
                     .setAutoCancel(true)
                     .setNumber(dialogKey.story ? storyPushMessages.size() : messageObjects.size())
-                    .setColor(0xff11acfa)
+                    .setColor(0xff3A2E5C)
                     .setGroupSummary(false)
                     .setWhen(date)
                     .setShowWhen(true)
