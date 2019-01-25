566dcf8fd857cbf9c773472e0870ced93a086248
thermatk <thermatk@thermatk.com>
2019-01-25 22:55:45 +0100

    [TF][KILL] remaining GMS(GCM, Wear, etc), except for Maps

--- TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
@@ -35,8 +35,6 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.multidex.MultiDex;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 
 import org.json.JSONObject;
 import org.telegram.messenger.voip.VideoCapturerDevice;
@@ -268,7 +266,7 @@ public class ApplicationLoader extends Application {
         }
 
         ApplicationLoader app = (ApplicationLoader) ApplicationLoader.applicationContext;
-        app.initPushServices();
+        //app.initPushServices();
         if (BuildVars.LOGS_ENABLED) {
             FileLog.d("app initied");
         }
@@ -384,7 +382,7 @@ public class ApplicationLoader extends Application {
             e.printStackTrace();
         }
     }
-
+/*
     private void initPushServices() {
         AndroidUtilities.runOnUIThread(() -> {
             if (getPushProvider().hasServices()) {
@@ -408,6 +406,7 @@ public class ApplicationLoader extends Application {
         }
         return true;
     }
+*/
 
     private static long lastNetworkCheck = -1;
     private static void ensureCurrentNetworkGet() {
