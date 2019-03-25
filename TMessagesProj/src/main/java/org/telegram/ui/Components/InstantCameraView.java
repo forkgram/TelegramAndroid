7c417dff82844b15ac9fc83b288d59689ecc4ad9
23rd <23rd@vivaldi.net>
2019-03-25 22:11:45 +0300

    Added setting to set default camera for Video Messages.

--- TMessagesProj/src/main/java/org/telegram/ui/Components/InstantCameraView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/InstantCameraView.java
@@ -657,7 +657,8 @@ public class InstantCameraView extends FrameLayout implements NotificationCenter
         cameraReady = false;
         selectedCamera = null;
         if (!fromPaused) {
-            isFrontface = true;
+            android.content.SharedPreferences preferences = org.telegram.messenger.MessagesController.getGlobalMainSettings();
+            isFrontface = !(preferences.getBoolean("rearVideoMessages", false));
             recordedTime = 0;
             progress = 0;
         }
