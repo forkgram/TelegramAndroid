e62a4e5844155dc8e4c66e9c8f21c83fde8b794d
23rd <23rd@vivaldi.net>
2019-08-22 22:17:05 +0300

    Added ability to open calendar from long click of on floating date.

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -7128,6 +7128,15 @@ public class ChatActivity extends BaseFragment implements
             pendingRequestsDelegate.setChatInfo(chatInfo, false);
         }
 
+        floatingDateView.setOnLongClickListener(view -> {
+            if (getParentActivity() == null) {
+                return false;
+            }
+            AndroidUtilities.hideKeyboard(searchItem.getSearchField());
+            showDialog(AlertsCreator.createCalendarPickerDialog(getParentActivity(), 1375315200000L, this::jumpToDate, themeDelegate).create());
+            return true;
+        });
+
         pinnedMessageView = null;
 
         undoView = null;
