55d1cc0a0f0539d65fd12a3f8391e127b3384d42
23rd <23rd@vivaldi.net>
2020-07-21 00:10:49 +0300

    Added option to disable thumbs in dialog list.

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
@@ -1684,6 +1684,10 @@ public class DialogCell extends BaseCell implements StoriesListPlaceProvider.Ava
                             }
                         }
                     } else {
+                        final boolean disableThumbs = MessagesController
+                            .getGlobalMainSettings()
+                            .getBoolean("disableThumbsInDialogList", false);
+
                         String restrictionReason = MessagesController.getInstance(message.currentAccount).getRestrictionReason(message.messageOwner.restriction_reason);
                         TLRPC.User fromUser = null;
                         TLRPC.Chat fromChat = null;
@@ -1834,6 +1838,7 @@ public class DialogCell extends BaseCell implements StoriesListPlaceProvider.Ava
                                         messageString = new SpannableStringBuilder(messageString);
                                     }
                                     checkMessage = false;
+                                    if (!disableThumbs) {
                                     SpannableStringBuilder builder = (SpannableStringBuilder) messageString;
                                     if (thumbInsertIndex >= builder.length()) {
                                         builder.append(" ");
@@ -1841,6 +1846,7 @@ public class DialogCell extends BaseCell implements StoriesListPlaceProvider.Ava
                                     } else {
                                         builder.insert(thumbInsertIndex, " ");
                                         builder.setSpan(new FixedWidthSpan(dp(thumbsCount * (thumbSize + 2) - 2 + 5)), thumbInsertIndex, thumbInsertIndex + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                                    }
                                     }
                                 }
                             } else {
@@ -2031,6 +2037,7 @@ public class DialogCell extends BaseCell implements StoriesListPlaceProvider.Ava
                                         messageString = new SpannableStringBuilder(messageString);
                                     }
                                     checkMessage = false;
+                                    if (!disableThumbs) {
                                     SpannableStringBuilder builder = (SpannableStringBuilder) messageString;
                                     builder.insert(0, " ");
                                     builder.setSpan(new FixedWidthSpan(dp((thumbSize + 2) * thumbsCount - 2 + 5)), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
@@ -2040,6 +2047,7 @@ public class DialogCell extends BaseCell implements StoriesListPlaceProvider.Ava
                                         if (s != null) {
                                             messageString = s;
                                         }
+                                    }
                                     }
                                 }
                                 if (message.isForwarded() && message.needDrawForwarded()) {
@@ -4609,7 +4617,10 @@ public class DialogCell extends BaseCell implements StoriesListPlaceProvider.Ava
                 canvas.restore();
             }
 
-            if (thumbsCount > 0 && updateHelper.typingProgres != 1f) {
+            if (thumbsCount > 0 && updateHelper.typingProgres != 1f
+                && !MessagesController
+                    .getGlobalMainSettings()
+                    .getBoolean("disableThumbsInDialogList", false)) {
                 float alpha = 1f;
                 if (updateHelper.typingProgres > 0) {
                     alpha = (1f - updateHelper.typingProgres);
@@ -5648,6 +5659,12 @@ public class DialogCell extends BaseCell implements StoriesListPlaceProvider.Ava
 
     public void updateMessageThumbs() {
         if (message == null) {
+            return;
+        }
+        final boolean disableThumbs = MessagesController
+            .getGlobalMainSettings()
+            .getBoolean("disableThumbsInDialogList", false);
+        if (disableThumbs) {
             return;
         }
         String restrictionReason = MessagesController.getInstance(message.currentAccount).getRestrictionReason(message.messageOwner.restriction_reason);
