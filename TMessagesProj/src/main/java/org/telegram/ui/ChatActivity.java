4fc468d91799eb18dfef8b00bf23f6fd968d0b74
23rd <23rd@vivaldi.net>
2020-09-16 02:48:26 +0300

    Added ability to copy links without protocol.

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -42479,6 +42479,10 @@ public class ChatActivity extends BaseFragment implements
             }
         });
 
+        options.add(R.drawable.msg_copy, getString(R.string.CopyWithoutProtocol), () -> {
+            AndroidUtilities.addToClipboard(str.replace("http://", "").replace("https://", ""));
+        });
+
         dialog.setItemOptions(options);
         if (str != null && str.startsWith("mailto:")) {
             SpannableString s = new SpannableString(str.substring(7));
