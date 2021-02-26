67e15e67cc312a7ac3971d39591f469df29ba92e
23rd <23rd@vivaldi.net>
2021-02-26 18:47:44 +0300

    Added ability to attach text to voice message before sending it.
    Added ability to append timestamps for voice messages.

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ChatActivityEnterView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ChatActivityEnterView.java
@@ -735,6 +735,7 @@ public class ChatActivityEnterView extends FrameLayout implements
     private boolean allowGifs;
 
     private boolean skipDotAtEnd = false;
+    private String voiceCaption = null;
 
     private int lastSizeChangeValue1;
     private boolean lastSizeChangeValue2;
@@ -7369,6 +7370,7 @@ public class ChatActivityEnterView extends FrameLayout implements
                 }
                 applyStoryToSendMessageParams(params);
                 SendMessagesHelper.getInstance(currentAccount).sendMessage(params);
+            voiceCaption = null;
                 if (delegate != null) {
                     delegate.onMessageSend(null, notify, scheduleDate, scheduleRepeatPeriod, payStars);
                 }
@@ -9819,6 +9821,7 @@ public class ChatActivityEnterView extends FrameLayout implements
         recordTimeContainer.addView(recordTimerView = new TimerView(getContext()), LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT, Gravity.CENTER_VERTICAL, 6, 0, 0, 0));
 
         recordPanel.addView(recordTimeContainer, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT, Gravity.CENTER_VERTICAL));
+        slideText.bringToFront();
     }
 
     @Override
@@ -14365,8 +14368,28 @@ public class ChatActivityEnterView extends FrameLayout implements
         final float replaceDistance = dp(15);
         float left;
 
+        public ArrayList<String> timestamps = new ArrayList<String>();
+
         public TimerView(Context context) {
             super(context);
+            
+            setOnClickListener((v) -> {
+                final String current = oldString.substring(0, oldString.indexOf(','));
+                timestamps.add(current);
+                
+                android.widget.Toast.makeText(
+                        parentActivity,
+                        "Saved timestamp at " + current + ".",
+                        android.widget.Toast.LENGTH_SHORT).show();
+                
+                android.content.ClipboardManager clipboard
+                    = (android.content.ClipboardManager) context.getSystemService(
+                        android.content.Context.CLIPBOARD_SERVICE);
+                String timestampsText = String.join("\n", timestamps);
+                android.content.ClipData clip
+                    = android.content.ClipData.newPlainText("Timestamps", timestampsText);
+                clipboard.setPrimaryClip(clip);
+            });
         }
 
         public void start(long milliseconds) {
@@ -14375,6 +14398,8 @@ public class ChatActivityEnterView extends FrameLayout implements
             externalElapsedMs = milliseconds;
             lastSendTypingTime = startTime;
             invalidate();
+            timestamps.clear();
+            timestamps.add("0:00");
         }
 
         void setExternalFrameClock(boolean enabled) {
