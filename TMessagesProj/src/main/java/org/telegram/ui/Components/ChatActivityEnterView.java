118d686bc92b59c8901237673193c6415f5247dd
23rd <23rd@vivaldi.net>
2022-06-20 23:36:39 +0300

    Disabled sending message when key Shift is pressed.

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ChatActivityEnterView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ChatActivityEnterView.java
@@ -5689,6 +5689,9 @@ public class ChatActivityEnterView extends FrameLayout implements
 
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if (keyEvent != null && keyEvent.isCtrlPressed()) {
+                    return false;
+                } else
                 if (i == EditorInfo.IME_ACTION_SEND) {
                     sendMessage();
                     return true;
