092c8715c657157886d9882e0cda34399f1d3393
23rd <23rd@vivaldi.net>
2023-02-03 23:55:03 +0300

    Added option to disable parameters from bot links.

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -2188,6 +2188,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
                                                 if (messageId != null) {
                                                     videoTimestamp = getTimestampFromLink(data);
                                                 }
+                                                if (!org.telegram.messenger.MessagesController.getGlobalMainSettings().getBoolean("disableParametersFromBotLinks", false)) {
                                                 botUser = data.getQueryParameter("start");
                                                 botChat = data.getQueryParameter("startgroup");
                                                 if (!TextUtils.isEmpty(username)) {
@@ -2210,6 +2211,7 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
                                                 }
                                                 botChannel = data.getQueryParameter("startchannel");
                                                 botChatAdminParams = data.getQueryParameter("admin");
+                                                }
                                                 game = data.getQueryParameter("game");
                                                 openDirect = data.getBooleanQueryParameter("direct", false);
                                                 voicechat = data.getQueryParameter("voicechat");
@@ -2292,10 +2294,12 @@ public class LaunchActivity extends BasePermissionsActivity implements INavigati
                                             auth.put("public_key", data.getQueryParameter("public_key"));
                                             auth.put("callback_url", data.getQueryParameter("callback_url"));
                                         } else {
+                                            if (!org.telegram.messenger.MessagesController.getGlobalMainSettings().getBoolean("disableParametersFromBotLinks", false)) {
                                             botUser = data.getQueryParameter("start");
                                             botChat = data.getQueryParameter("startgroup");
                                             botChannel = data.getQueryParameter("startchannel");
                                             botChatAdminParams = data.getQueryParameter("admin");
+                                            }
                                             if (data.getQueryParameter("task") != null) {
                                                 taskId = Utilities.parseInt(data.getQueryParameter("task"));
                                             }
