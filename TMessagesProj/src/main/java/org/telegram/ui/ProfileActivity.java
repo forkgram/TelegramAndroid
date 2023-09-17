e4dd8a4cb71b069648356ad54027e1d2caba6bca
thermatk <thermatk@thermatk.com>
2023-09-17 23:32:26 +0200

    [TF][KILL] Google Translate without MLKit

--- TMessagesProj/src/main/java/org/telegram/ui/ProfileActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ProfileActivity.java
@@ -149,7 +149,6 @@ import org.telegram.messenger.FlagSecureReason;
 import org.telegram.messenger.ImageLoader;
 import org.telegram.messenger.ImageLocation;
 import org.telegram.messenger.ImageReceiver;
-import org.telegram.messenger.LanguageDetector;
 import org.telegram.messenger.LiteMode;
 import org.telegram.messenger.LocaleController;
 import org.telegram.messenger.MediaController;
@@ -7581,7 +7580,7 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
                 popupWindow.dimBehind();
             };
             if (withTranslate[0]) {
-                if (LanguageDetector.hasSupport()) {
+                /*if (LanguageDetector.hasSupport()) {
                     LanguageDetector.detectLanguage(finalText, (fromLang) -> {
                         fromLanguage[0] = fromLang;
                         withTranslate[0] = fromLang != null && (!fromLang.equals(toLang) || fromLang.equals("und")) && (
@@ -7592,9 +7591,9 @@ public class ProfileActivity extends BaseFragment implements NotificationCenter.
                         FileLog.e("mlkit: failed to detect language in selection", error);
                         showMenu.run();
                     });
-                } else {
+                } else {*/
                     showMenu.run();
-                }
+                //}
             } else {
                 showMenu.run();
             }
