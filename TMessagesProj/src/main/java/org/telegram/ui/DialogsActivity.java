a7141a057d4548f24b4b58e15ca2ba9caca051a6
23rd <23rd@vivaldi.net>
2023-09-18 14:13:53 +0300

    Removed useless "New message" floating button.

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -5501,6 +5501,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
         contentView.addView(rightSlidingDialogContainer, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, LayoutHelper.MATCH_PARENT));
         contentView.addView(dialogStoriesCell, LayoutHelper.createFrame(LayoutHelper.MATCH_PARENT, DialogStoriesCell.HEIGHT_IN_DP));
         updateStoriesVisibility(false);
+        hideFloatingButton(true);
         return fragmentView;
     }
 
@@ -11314,6 +11315,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
     }
 
     private void hideFloatingButton(boolean hide) {
+        hide = true;
         if (rightSlidingDialogContainer.hasFragment()) {
             hide = true;
         }
