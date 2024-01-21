28145d4b796bb16d54e9975eb837c8c94cf8e13e
thermatk <thermatk@thermatk.com>
2024-01-21 14:26:23 +0100

    [TF][KILL] Android Billing

--- TMessagesProj/src/main/java/org/telegram/ui/Components/Premium/boosts/UserSelectorBottomSheet.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/Premium/boosts/UserSelectorBottomSheet.java
@@ -466,7 +466,7 @@ public class UserSelectorBottomSheet extends BottomSheetWithRecyclerListView imp
         initHints(false);
         updateList(false, true);
         fixNavigationBar();
-        if (type == TYPE_PREMIUM || type == TYPE_STAR_GIFT) {
+        /*if (type == TYPE_PREMIUM || type == TYPE_STAR_GIFT) {
             BoostRepository.loadGiftOptions(currentAccount, null, arg -> {
                 paymentOptions.clear();
                 paymentOptions.addAll(arg);
@@ -477,7 +477,7 @@ public class UserSelectorBottomSheet extends BottomSheetWithRecyclerListView imp
                     }
                 }
             });
-        }
+        }*/
         if (type == TYPE_PREMIUM || type == TYPE_STAR_GIFT) {
             StarsController.getInstance(currentAccount).loadStarGifts();
         }
