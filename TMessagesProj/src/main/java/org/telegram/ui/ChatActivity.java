ce6a102511ab00d3f55b940f0a82966d35e3c840
thermatk <thermatk@thermatk.com>
2024-03-03 13:24:54 +0100

    [TF][KILL] Huawei

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -36165,7 +36165,6 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                         LaunchActivity.instance.checkAppUpdate(true, progressDialogCurrent);
                     }
                 } else if (BuildVars.isHuaweiStoreApp()){
-                    Browser.openUrl(getContext(), BuildVars.HUAWEI_STORE_URL);
                 } else {
                     Browser.openUrl(getContext(), BuildVars.PLAYSTORE_APP_URL);
                 }
