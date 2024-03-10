a82fcf0c622d0132c399f2f51dfd0d1e7e7f787d
thermatk <thermatk@thermatk.com>
2024-03-11 00:03:22 +0100

    [TF][KILL] more Google Vision

--- TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
@@ -144,10 +144,6 @@ import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.analytics.AnalyticsListener;
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout;
 import com.google.android.exoplayer2.video.VideoSize;
-import com.google.android.gms.cast.framework.CastContext;
-import com.google.android.gms.vision.Frame;
-import com.google.android.gms.vision.face.Face;
-import com.google.android.gms.vision.face.FaceDetector;
 import org.telegram.messenger.AccountInstance;
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.AnimationNotificationsLocker;
@@ -11669,7 +11665,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
         if (key == null || bitmap == null || bitmap.bitmap == null) {
             return;
         }
-        Utilities.globalQueue.postRunnable(() -> {
+        /*Utilities.globalQueue.postRunnable(() -> {
             FaceDetector faceDetector = null;
             try {
                 faceDetector = new FaceDetector.Builder(ApplicationLoader.applicationContext)
@@ -11707,7 +11703,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
                     faceDetector.release();
                 }
             }
-        });
+        });*/
     }
 
     private boolean wasCountViewShown;
