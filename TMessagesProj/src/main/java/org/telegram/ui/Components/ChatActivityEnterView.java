90baf05a5590a59660638e3d3694ea9cc067b97f
23rd <23rd@vivaldi.net>
2024-06-04 01:46:11 +0300

    Attempted to remove Android Billing.

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ChatActivityEnterView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ChatActivityEnterView.java
@@ -3648,7 +3648,7 @@ public class ChatActivityEnterView extends FrameLayout implements
             final boolean birthday = getParentFragment().getCurrentUserInfo() != null && BirthdayController.isToday(getParentFragment().getCurrentUserInfo().birthday);
             final AlertDialog progressDialog = new AlertDialog(getContext(), AlertDialog.ALERT_TYPE_SPINNER);
             progressDialog.showDelayed(200);
-            final int reqId = BoostRepository.loadGiftOptions(currentAccount, null, loadedOptions -> {
+            /*final int reqId = BoostRepository.loadGiftOptions(currentAccount, null, loadedOptions -> {
                 progressDialog.dismiss();
                 loadedOptions = BoostRepository.filterGiftOptions(loadedOptions, 1);
                 loadedOptions = BoostRepository.filterGiftOptionsByBilling(loadedOptions);
@@ -3656,7 +3656,7 @@ public class ChatActivityEnterView extends FrameLayout implements
             });
             progressDialog.setOnCancelListener(di -> {
                 parentFragment.getConnectionsManager().cancelRequest(reqId, true);
-            });
+            });*/
         });
     }
 
