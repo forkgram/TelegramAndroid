bb03a1a5060a5a106723b6e6af322d2b8f103371
23rd <23rd@vivaldi.net>
2025-03-12 01:18:39 +0300

    Attempted to remove Chromecast.

--- TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
@@ -187,7 +187,7 @@ import org.telegram.messenger.VideoEditedInfo;
 import org.telegram.messenger.WebFile;
 import org.telegram.messenger.browser.Browser;
 import org.telegram.messenger.camera.Size;
-import org.telegram.messenger.chromecast.ChromecastController;
+//import org.telegram.messenger.chromecast.ChromecastController;
 import org.telegram.messenger.chromecast.ChromecastMedia;
 import org.telegram.messenger.chromecast.ChromecastMediaVariations;
 import org.telegram.messenger.pip.source.IPipSourceDelegate;
@@ -5124,7 +5124,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
                         dialog.setTextColor(getThemedColor(Theme.key_voipgroup_actionBarItems));
                     }
                 } else if (id == gallery_menu_chromecast) {
-                    ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
+//                    ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
                     castItemButton.performClick();
                 } else if (id == gallery_menu_showall) {
                     if (currentDialogId != 0) {
@@ -5843,7 +5843,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
         };
         boolean castAvailable = true;
         try {
-            castItemButton.setRouteSelector(CastContext.getSharedInstance(activityContext).getMergedSelector());
+//            castItemButton.setRouteSelector(CastContext.getSharedInstance(activityContext).getMergedSelector());
         } catch (Exception e) {
             FileLog.e(e);
             castAvailable = false;
@@ -8474,9 +8474,9 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
         videoItem.toggleSubMenu();
         try {
             CastSync.check(CastSync.TYPE_PHOTOVIEWER);
-            if (ChromecastController.getInstance().isCasting()) {
-                ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
-            }
+            //if (ChromecastController.getInstance().isCasting()) {
+            //    ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
+            //}
             if (videoPlayer != null) {
                 CastSync.setPlaying(videoPlayer.isPlaying());
             }
@@ -8686,9 +8686,9 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
         if (lastQualityIndexSelected != qualityIndexSelected) {
             try {
                 CastSync.check(CastSync.TYPE_PHOTOVIEWER);
-                if (ChromecastController.getInstance().isCasting()) {
-                    ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
-                }
+                //if (ChromecastController.getInstance().isCasting()) {
+                //    ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
+                //}
                 if (videoPlayer != null) {
                     CastSync.setPlaying(videoPlayer.isPlaying());
                 }
@@ -14442,9 +14442,9 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
         lastQualityIndexSelected = videoPlayer != null ? videoPlayer.getCurrentQualityIndex() : -1;
         try {
             CastSync.check(CastSync.TYPE_PHOTOVIEWER);
-            if (ChromecastController.getInstance().isCasting()) {
-                ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
-            }
+            //if (ChromecastController.getInstance().isCasting()) {
+            //    ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
+            //}
             if (videoPlayer != null) {
                 CastSync.setPlaying(videoPlayer.isPlaying());
             }
@@ -19276,9 +19276,9 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
 
         try {
             CastSync.check(CastSync.TYPE_PHOTOVIEWER);
-            if (ChromecastController.getInstance().isCasting()) {
-                ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
-            }
+            //if (ChromecastController.getInstance().isCasting()) {
+            //    ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
+            //}
         } catch (Exception e) {
             FileLog.e(e);
         }
