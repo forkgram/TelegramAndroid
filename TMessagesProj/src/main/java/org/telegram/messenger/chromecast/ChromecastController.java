922dcf9658e767e59eb85b5d0f42ebc780956f3b
23rd <23rd@vivaldi.net>
2025-03-12 01:18:39 +0300

    Attempted to remove Chromecast.

--- TMessagesProj/src/main/java/org/telegram/messenger/chromecast/ChromecastController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/chromecast/ChromecastController.java
@@ -5,16 +5,16 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import com.google.android.gms.cast.CastDevice;
-import com.google.android.gms.cast.MediaError;
-import com.google.android.gms.cast.MediaLoadOptions;
-import com.google.android.gms.cast.MediaMetadata;
-import com.google.android.gms.cast.MediaStatus;
-import com.google.android.gms.cast.framework.CastContext;
-import com.google.android.gms.cast.framework.CastSession;
-import com.google.android.gms.cast.framework.SessionManager;
-import com.google.android.gms.cast.framework.SessionManagerListener;
-import com.google.android.gms.cast.framework.media.RemoteMediaClient;
+//import com.google.android.gms.cast.CastDevice;
+//import com.google.android.gms.cast.MediaError;
+//import com.google.android.gms.cast.MediaLoadOptions;
+//import com.google.android.gms.cast.MediaMetadata;
+//import com.google.android.gms.cast.MediaStatus;
+//import com.google.android.gms.cast.framework.CastContext;
+//import com.google.android.gms.cast.framework.CastSession;
+//import com.google.android.gms.cast.framework.SessionManager;
+//import com.google.android.gms.cast.framework.SessionManagerListener;
+//import com.google.android.gms.cast.framework.media.RemoteMediaClient;
 
 import org.telegram.messenger.ApplicationLoader;
 import org.telegram.ui.CastSync;
@@ -22,7 +22,7 @@ import org.telegram.ui.PhotoViewer;
 
 import java.io.File;
 import java.util.Objects;
-
+/*
 public class ChromecastController implements SessionManagerListener<CastSession> {
     private final static String CAST_CONTROLLER = "CAST_CONTROLLER";
     private final static String CAST_SESSION_TAG = "CAST_SESSION";
@@ -112,7 +112,6 @@ public class ChromecastController implements SessionManagerListener<CastSession>
     }
 
 
-    /* * */
 
     private static volatile ChromecastController Instance = null;
 
@@ -131,7 +130,6 @@ public class ChromecastController implements SessionManagerListener<CastSession>
 
 
 
-    /* * */
 
     static class RemoteMediaClientHandler extends RemoteMediaClient.Callback {
         public final RemoteMediaClient client;
@@ -257,7 +255,6 @@ public class ChromecastController implements SessionManagerListener<CastSession>
 
 
 
-    /* Session Listener */
 
     @Override
     public void onSessionStarting(@NonNull CastSession castSession) {
@@ -308,3 +305,4 @@ public class ChromecastController implements SessionManagerListener<CastSession>
     }
 
 }
+*/
--- TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
@@ -184,7 +184,7 @@ import org.telegram.messenger.VideoEditedInfo;
 import org.telegram.messenger.WebFile;
 import org.telegram.messenger.browser.Browser;
 import org.telegram.messenger.camera.Size;
-import org.telegram.messenger.chromecast.ChromecastController;
+//import org.telegram.messenger.chromecast.ChromecastController;
 import org.telegram.messenger.chromecast.ChromecastMedia;
 import org.telegram.messenger.chromecast.ChromecastMediaVariations;
 import org.telegram.messenger.video.OldVideoPlayerRewinder;
@@ -5743,7 +5743,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
         };
         boolean castAvailable = true;
         try {
-            castItemButton.setRouteSelector(CastContext.getSharedInstance(activityContext).getMergedSelector());
+//            castItemButton.setRouteSelector(CastContext.getSharedInstance(activityContext).getMergedSelector());
         } catch (Exception e) {
             FileLog.e(e);
             castAvailable = false;
@@ -8301,7 +8301,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
         videoItem.toggleSubMenu();
         try {
             CastSync.check(CastSync.TYPE_PHOTOVIEWER);
-            ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
+//            ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
             if (videoPlayer != null) {
                 CastSync.setPlaying(videoPlayer.isPlaying());
             }
@@ -8511,7 +8511,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
         if (lastQualityIndexSelected != qualityIndexSelected) {
             try {
                 CastSync.check(CastSync.TYPE_PHOTOVIEWER);
-                ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
+//                ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
                 if (videoPlayer != null) {
                     CastSync.setPlaying(videoPlayer.isPlaying());
                 }
@@ -14180,7 +14180,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
         lastQualityIndexSelected = videoPlayer != null ? videoPlayer.getCurrentQualityIndex() : -1;
         try {
             CastSync.check(CastSync.TYPE_PHOTOVIEWER);
-            ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
+//            ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
             if (videoPlayer != null) {
                 CastSync.setPlaying(videoPlayer.isPlaying());
             }
@@ -18889,7 +18889,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
 
         try {
             CastSync.check(CastSync.TYPE_PHOTOVIEWER);
-            ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
+//            ChromecastController.getInstance().setCurrentMediaAndCastIfNeeded(getCurrentChromecastMedia());
         } catch (Exception e) {
             FileLog.e(e);
         }
