33cc114164999cfa9ed43f5c39d021358c46f57e
23rd <23rd@vivaldi.net>
2025-03-12 01:18:39 +0300

    Attempted to remove Chromecast.

--- TMessagesProj/src/main/java/org/telegram/ui/Components/AudioPlayerAlert.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/AudioPlayerAlert.java
@@ -61,7 +61,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.exoplayer2.C;
-import com.google.android.gms.cast.framework.CastContext;
+//import com.google.android.gms.cast.framework.CastContext;
 
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.ApplicationLoader;
@@ -1165,7 +1165,7 @@ public class AudioPlayerAlert extends BottomSheet implements NotificationCenter.
         };
         boolean castAvailable = true;
         try {
-            castItemButton.setRouteSelector(CastContext.getSharedInstance(context).getMergedSelector());
+//            castItemButton.setRouteSelector(CastContext.getSharedInstance(context).getMergedSelector());
         } catch (Exception e) {
             FileLog.e(e);
             castAvailable = false;
