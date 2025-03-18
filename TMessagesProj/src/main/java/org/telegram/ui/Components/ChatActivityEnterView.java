db7e15ae2ef58deeae731136dbe740dc1309ab52
23rd <23rd@vivaldi.net>
2025-03-18 18:38:15 +0300

    Removed gift button from input message field.

--- TMessagesProj/src/main/java/org/telegram/ui/Components/ChatActivityEnterView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/ChatActivityEnterView.java
@@ -9603,6 +9603,7 @@ public class ChatActivityEnterView extends BlurredFrameLayout implements Notific
         final TLRPC.UserFull myUserInfo = MessagesController.getInstance(currentAccount).getUserFull(UserConfig.getInstance(currentAccount).getClientUserId());
         final TLRPC.User user = getParentFragment() == null ? null : getParentFragment().getCurrentUser();
         final boolean visible =
+            false &&
             !MessagesController.getInstance(currentAccount).premiumPurchaseBlocked() &&
             getParentFragment() != null && user != null &&
             !BuildVars.IS_BILLING_UNAVAILABLE &&
