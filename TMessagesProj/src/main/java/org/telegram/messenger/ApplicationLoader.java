13989358a8679869f9269152110f40eeabdba4ff
Kryštof Baksa <62813600+Golbinex@users.noreply.github.com>
2025-06-05 00:04:41 +0200

    UnifiedPush support PR by @Golbinex
    * [TF][PUSH] implemented a simple UnifiedPush support
    
    This simple implementation only triggers the networks to resume when an
    event is received, so Telegram can get the events by itself.
    
    This is not like the other push providers where the change information
    is processed by PushListenerController directly, but it still saves battery
    since the process disconnects from Telegram servers as soon as the
    updates are processed.
    
    * [MG] add a longclick listener to UnifiedPush Distributors menu
    
    This prints informations to know if UnifiedPush is working or not.
    
    * [MG] encode the url to pass to p2p.belloworld.it
    
    This is needed if urls includes query (like UP-FCM).
    
    This also means the proxy had to be a little more complex, since nginx didn't do url decoding:
    https://gist.github.com/drizzt/93d253bca4f64fae7df2a4544a98c08d
    
    ---------
    
    Co-authored-by: Timothy Redaelli <timothy@fsfe.org>

--- TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/ApplicationLoader.java
@@ -94,7 +94,7 @@ public class ApplicationLoader extends Application {
     }
 
     protected PushListenerController.IPushListenerServiceProvider onCreatePushProvider() {
-        return PushListenerController.GooglePushListenerServiceProvider.INSTANCE;
+        return PushListenerController.UnifiedPushListenerServiceProvider.INSTANCE;
     }
 
     public static String getApplicationId() {
@@ -242,7 +242,7 @@ public class ApplicationLoader extends Application {
         }
 
         ApplicationLoader app = (ApplicationLoader) ApplicationLoader.applicationContext;
-        //app.initPushServices();
+        app.initPushServices();
         if (BuildVars.LOGS_ENABLED) {
             FileLog.d("app initied");
         }
@@ -429,7 +429,6 @@ public class ApplicationLoader extends Application {
             e.printStackTrace();
         }
     }
-/*
     private void initPushServices() {
         AndroidUtilities.runOnUIThread(() -> {
             if (getPushProvider().hasServices()) {
@@ -443,7 +442,7 @@ public class ApplicationLoader extends Application {
             }
         }, 1000);
     }
-
+/*
     private boolean checkPlayServices() {
         try {
             int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
