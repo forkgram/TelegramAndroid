c8cf341799fd966306fbb4d3db39ef747abeb4e3
23rd <23rd@vivaldi.net>
2025-09-20 13:29:25 +0300

    Added option to delete all user messages in all forum topics at once.

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -1758,6 +1758,7 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
     private final static int goToFirstMessage = 35;
     private final static int deleteAllYourMessages = 36;
     private final static int deleteAllUnpinnedMessages = 37;
+    private final static int deleteAllYourMessagesInAllTopics = 38;
 
     private final static int attach_photo = 0;
     private final static int attach_gallery = 1;
@@ -4190,6 +4191,11 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                         currentAccount,
                         dialog_id,
                         getParentActivity());
+                } else if (id == deleteAllYourMessagesInAllTopics) {
+                    org.telegram.messenger.forkgram.ForkDialogs.CreateDeleteAllYourMessagesInAllTopicsAlert(
+                        currentAccount,
+                        dialog_id,
+                        getParentActivity());
                 } else if (id == deleteAllUnpinnedMessages) {
                     org.telegram.messenger.forkgram.ForkDialogs.CreateDeleteAllUnpinnedMessagesAlert(
                         currentAccount,
@@ -4530,6 +4536,14 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     R.drawable.msg_delete,
                     LocaleController.getString("DeleteAllYourMessages", R.string.DeleteAllYourMessages),
                     themeDelegate);
+                // Add option to delete messages in all topics for forum groups
+                if (ChatObject.isForum(currentChat)) {
+                    headerItem.addSubItem(
+                        deleteAllYourMessagesInAllTopics,
+                        R.drawable.msg_delete,
+                        LocaleController.getString("DeleteAllYourMessagesInAllTopics", R.string.DeleteAllYourMessagesInAllTopics),
+                        themeDelegate);
+                }
             }
             if (MessagesController.getGlobalMainSettings().getBoolean("addItemToDeleteAllUnpinnedMessages", false)
                 && ((currentUser != null && currentEncryptedChat == null) || currentChat != null)) {
