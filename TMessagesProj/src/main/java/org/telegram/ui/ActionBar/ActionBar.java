c3cb6a460a5922e9879f0a4c6cda5e501c7db885
23rd <23rd@vivaldi.net>
2026-02-15 22:38:20 +0300

    Added ability to open Saved Messages on long click on search top button.

--- TMessagesProj/src/main/java/org/telegram/ui/ActionBar/ActionBar.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ActionBar/ActionBar.java
@@ -165,6 +165,7 @@ public class ActionBar extends FrameLayout implements FactorAnimator.Target, The
     private boolean attachState;
     private FrameLayout titlesContainer;
     private boolean useContainerForTitles;
+    private OnLongClickListener titleLongClickListener;
 
     private View.OnTouchListener interceptTouchEventListener;
     private final Theme.ResourcesProvider resourcesProvider;
@@ -1662,6 +1663,11 @@ public class ActionBar extends FrameLayout implements FactorAnimator.Target, The
 
     public void setTitleActionRunnable(Runnable action) {
         lastRunnable = titleActionRunnable = action;
+    }
+
+    public void setTitleLongClickListener(OnLongClickListener listener) {
+        titleLongClickListener = listener;
+        setOnLongClickListener(listener);
     }
 
     boolean overlayTitleAnimationInProgress;
