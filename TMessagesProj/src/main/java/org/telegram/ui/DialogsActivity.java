bb4a095dd45ab329a9c3a9c6d41be7726bdb430d
23rd <23rd@vivaldi.net>
2026-02-15 22:38:20 +0300

    Added ability to open Saved Messages on long click on search top button.

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -3235,6 +3235,12 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
         ActionBarMenu menu = actionBar.createMenu();
         menu.setTranslationX(-dp(5));
         searchItem = menu.addItem(0, R.drawable.outline_header_search).setIsSearchField(true, false);
+        searchItem.setOnLongClickListener(v -> {
+            Bundle args = new Bundle();
+            args.putLong("user_id", UserConfig.getInstance(currentAccount).getClientUserId());
+            presentFragment(new ChatActivity(args));
+            return true;
+        });
         searchItem.setOnClickListener(v -> {
             showSearch(true, false, true);
             fragmentSearchFieldWatcher.toggleSearch(true);
@@ -3456,14 +3462,13 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
         if (initialDialogsType == DIALOGS_TYPE_DEFAULT) {
             optionsItem = menu.addItem(4, R.drawable.ic_ab_other);
             optionsItem.setContentDescription(LocaleController.getString(R.string.AccDescrMoreOptions));
+            optionsItem.setOnLongClickListener(v -> {
+                presentFragment(new SettingsActivity());
+                return true;
+            });
             optionsItem.setOnClickListener(v -> {
                 getContactsController().loadGlobalPrivacySetting();
                 showItemOptions();
-            });
-            optionsItem.setOnLongClickListener(v -> {
-                getContactsController().loadGlobalPrivacySetting();
-                showItemOptions();
-                return true;
             });
         }
 
@@ -3535,6 +3540,12 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
                 ssb.setSpan(new ImageSpan(logoDrawable), 0, ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
                 actionBar.setTitle(ssb, statusDrawable);
                 actionBar.setTitle(MessagesController.getGlobalMainSettings().getString("forkCustomTitle", "Fork Client"));
+                actionBar.setTitleLongClickListener(v -> {
+                    UserConfig.getInstance(currentAccount).setMainTabsHiddenFork(!UserConfig.getInstance(currentAccount).getMainTabsHiddenFork());
+                    checkUi_mainTabsVisible();
+                    return true;
+                });
+
                 updateStatus(UserConfig.getInstance(currentAccount).getCurrentUser(), false);
             }
             if (folderId == 0) {
@@ -13775,15 +13786,23 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
             });
         });
         io.addGap();
-        io.add(R.drawable.outline_groups_24, getString(R.string.NewGroup), () -> {
-            Bundle args = new Bundle();
-            presentFragment(new GroupCreateActivity(args));
-        });
         io.add(R.drawable.outline_saved_24, getString(R.string.SavedMessages), () -> {
             Bundle args = new Bundle();
             args.putLong("user_id", UserConfig.getInstance(currentAccount).getClientUserId());
             presentFragment(new ChatActivity(args));
         });
+        io.add(R.drawable.outline_groups_24, getString(R.string.NewMessageTitle), () -> {
+            openWriteContacts();
+        });
+        if (MessagesController.getInstance(currentAccount).storiesEnabled()) {
+            io.add(R.drawable.outline_fab_story_24, getString(R.string.RecorderNewStory), () -> {
+                if (!MessagesController.getInstance(currentAccount).storiesEnabled()) {
+                    dialogStoriesCell.showPremiumHint();
+                } else {
+                    dialogStoriesCell.openStoryRecorder();
+                }
+            });
+        }
         if (ApplicationLoader.applicationLoaderInstance != null) {
             ApplicationLoader.applicationLoaderInstance.addItemOptions(io);
         }
@@ -14079,7 +14098,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
     private void checkUi_mainTabsVisible() {
         final boolean mainTabsVisible = !searching && (blurredView == null || blurredView.getBackground() == null || blurredView.getAlpha() < 0.01f || blurredView.getVisibility() == View.GONE);
         if (mainTabsActivityController != null) {
-            mainTabsActivityController.setTabsVisible(mainTabsVisible);
+            mainTabsActivityController.setTabsVisible(UserConfig.getInstance(currentAccount).getMainTabsHiddenFork() ? false : mainTabsVisible);
         }
     }
 
