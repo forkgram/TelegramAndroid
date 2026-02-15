d785b41078e3f7492a8e7412dee37cf3bd106a93
23rd <23rd@vivaldi.net>
2026-02-15 22:38:20 +0300

    Added ability to open Saved Messages on long click on search top button.

--- TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/UserConfig.java
@@ -61,6 +61,7 @@ public class UserConfig extends BaseController {
     public boolean syncContacts = true;
     public boolean suggestContacts = true;
     public boolean showCallsTab;
+    public boolean mainTabsHiddenFork = false;
     public boolean hasSecureData;
     public int loginTime;
     public TLRPC.TL_help_termsOfService unacceptedTermsOfService;
@@ -159,6 +160,7 @@ public class UserConfig extends BaseController {
                     editor.putInt("loginTime", loginTime);
                     editor.putBoolean("syncContacts", syncContacts);
                     editor.putBoolean("showCallsTab", showCallsTab);
+                    editor.putBoolean("mainTabsHiddenFork", mainTabsHiddenFork);
                     editor.putBoolean("suggestContacts", suggestContacts);
                     editor.putBoolean("hasSecureData", hasSecureData);
                     editor.putBoolean("notificationsSettingsLoaded4", notificationsSettingsLoaded);
@@ -310,6 +312,7 @@ public class UserConfig extends BaseController {
             loginTime = preferences.getInt("loginTime", currentAccount);
             syncContacts = preferences.getBoolean("syncContacts", true);
             showCallsTab = preferences.getBoolean("showCallsTab", false);
+            mainTabsHiddenFork = preferences.getBoolean("mainTabsHiddenFork", false);
             suggestContacts = preferences.getBoolean("suggestContacts", true);
             hasSecureData = preferences.getBoolean("hasSecureData", false);
             notificationsSettingsLoaded = preferences.getBoolean("notificationsSettingsLoaded4", false);
@@ -481,6 +484,7 @@ public class UserConfig extends BaseController {
         contactsReimported = true;
         syncContacts = true;
         showCallsTab = false;
+        mainTabsHiddenFork = false;
         suggestContacts = true;
         unreadDialogsLoaded = true;
         hasValidDialogLoadIds = true;
@@ -563,6 +567,17 @@ public class UserConfig extends BaseController {
     public void setShowCallsTab(boolean show) {
         if (showCallsTab != show) {
             showCallsTab = show;
+            saveConfig(false);
+        }
+    }
+
+    public boolean getMainTabsHiddenFork() {
+        return mainTabsHiddenFork;
+    }
+
+    public void setMainTabsHiddenFork(boolean visible) {
+        if (mainTabsHiddenFork != visible) {
+            mainTabsHiddenFork = visible;
             saveConfig(false);
         }
     }
