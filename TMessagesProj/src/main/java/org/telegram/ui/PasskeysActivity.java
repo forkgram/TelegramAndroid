a593a66b090e5943afe65d88551450c1a994d243
optimumpro <1optimizerpro@gmail.com>
2026-02-28 13:29:41 -0500

    Revert "Disabled new passkeys."
    This reverts commit c64e3cf18c966046e9948eda06b2e0a572321b75.

--- TMessagesProj/src/main/java/org/telegram/ui/PasskeysActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PasskeysActivity.java
@@ -341,9 +341,6 @@ public class PasskeysActivity extends BaseFragment {
 
         ButtonWithCounterView button = new ButtonWithCounterView(context, resourcesProvider).setRound();
         button.setText(getString(R.string.PasskeyFeatureButton), false);
-        button.setText("Disabled for non-official apps", false);
-        button.setEnabled(false);
-        button.setClickable(false);
         button.setOnClickListener(v -> {
             if (button.isLoading()) return;
             button.setLoading(true);
@@ -403,8 +400,6 @@ public class PasskeysActivity extends BaseFragment {
                 }
             });
         });
-        button.setOnTouchListener((v, event) -> true);
-        button.setOnClickListener(null);
 
         if (withCreateButton) {
             linearLayout.addView(button, LayoutHelper.createLinear(LayoutHelper.MATCH_PARENT, 48, 0, 16, 0, 8));
