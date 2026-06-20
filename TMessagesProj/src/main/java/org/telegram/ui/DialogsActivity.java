909ef65864a23c34d2790d2e2c00eb2bedd389cc
23rd <23rd@vivaldi.net>
2026-06-21 00:48:10 +0300

    Added option to hide in-app hint banners.

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -6371,6 +6371,10 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
             dialogsHintCellVisible = false;
         }
 
+        if (MessagesController.getGlobalMainSettings().getBoolean("hideInAppHints", false)) {
+            dialogsHintCellVisible = false;
+        }
+
         topPanelLayout.setViewVisible(dialogsHintCell, dialogsHintCellVisible);
 
         checkCommunityPendingRequestsVisible(true);
