e1db320f227e8216cc469b8204a0e50feff1c432
23rd <23rd@vivaldi.net>
2026-06-21 00:48:09 +0300

    Added option to hide reactions shown on messages. (#274)

--- TMessagesProj/src/main/java/org/telegram/messenger/MessageObject.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessageObject.java
@@ -699,6 +699,9 @@ public class MessageObject {
         if (isRepostPreview) {
             return false;
         }
+        if (MessagesController.getGlobalMainSettings().getBoolean("hideMessageReactions", false)) {
+            return false;
+        }
         return true;
     }
 
