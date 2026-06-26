6e0edb9a2e523882475fd9ac59c2631fb29571b9
23rd <23rd@vivaldi.net>
2026-06-26 13:32:29 +0300

    Fixed bg chat scrolling when forward menu's keyboard opens.

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -18738,6 +18738,8 @@ public class ChatActivity extends BaseFragment implements
         protected void onMeasure(int origWidthMeasureSpec, int heightMeasureSpec) {
             invalidateBlurredSourcesView.bringToFrontIfNeeded();
 
+            updateChatListViewSuppression(false);
+
             final int allWidth = MeasureSpec.getSize(origWidthMeasureSpec);
             final int allHeight = MeasureSpec.getSize(heightMeasureSpec);
 
@@ -28114,6 +28116,19 @@ public class ChatActivity extends BaseFragment implements
         }
     }
 
+    private void updateChatListViewSuppression(boolean requestLayoutOnResume) {
+        if (chatListView == null || chatListView.isComputingLayout()) {
+            return;
+        }
+        final boolean freezeChatList = visibleDialog != null && visibleDialog.isShowing() || getLastSheet() != null;
+        if (chatListView.isLayoutSuppressed() != freezeChatList) {
+            chatListView.suppressLayout(freezeChatList);
+            if (!freezeChatList && requestLayoutOnResume) {
+                chatListView.requestLayout();
+            }
+        }
+    }
+
     @Override
     protected void onDialogDismiss(Dialog dialog) {
         if (closeChatDialog != null && dialog == closeChatDialog) {
@@ -28126,6 +28141,7 @@ public class ChatActivity extends BaseFragment implements
                 finishFragment();
             }
         }
+        AndroidUtilities.runOnUIThread(() -> updateChatListViewSuppression(true));
     }
 
     @Override
